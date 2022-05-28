003b47b6e40f91837169a2795eba408e11af293d
Omar Pulido <pulido.morales.omar@gmail.com>
2022-05-27 19:19:07 -0500

    navbar finalizado

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeComponent from '../pages/HomeComponent.vue'
+import FoldersComponent from '../pages/FoldersComponent.vue'
+import LeaksComponent from '../pages/LeaksComponent.vue'
+import TrashComponent from '../pages/TrashComponent.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeComponent
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/folders',
+    name: 'folders',
+    component: () => FoldersComponent
+  },
+  {
+    path: '/leaks',
+    name: 'leaks',
+    component: () => LeaksComponent
+  },
+  {
+    path: '/trash',
+    name: 'trash',
+    component: () => TrashComponent
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,37 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    navigation: [
+      { name: "Home", to: "/", current: true },
+      { name: "Folders", to: "/folders", current: false },
+      { name: "Leaks", to: "/leaks", current: false },
+      { name: "Trash", to: "/trash", current: false },
+    ],
   },
   getters: {
   },
   mutations: {
+    changeCurrent(state, index) {
+      if (!state.navigation[index].current) {
+        state.navigation[index].current = true;
+
+        state.navigation.forEach((element) =>
+          element !== state.navigation[index] ? (element.current = false) : {}
+        );
+      }
+    },
+
+    setCurrentAtRefresh(state) {
+
+      state.navigation.forEach((element, index) => {
+        if (window.location.href.includes(element.to) && element.to.length > 1) {
+          state.navigation[index].current = true;
+        }
+        else {
+          state.navigation[index].current = false;
+        }
+      });
+    }
   },
   actions: {
   },
