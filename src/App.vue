aff5080af2b8c2893e51e61a77ae8759a24b9d38
omarf045 <pulido.morales.omar@gmail.com>
2022-06-04 19:11:59 -0500

    navigation completed

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="home h-full">
+    <Bar />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+import Bar from '@/components/BarComponent.vue'
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+  components: {
+    Bar
+  }
 }
+</script>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,52 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import {
+  CogIcon,
+  TrashIcon,
+  HomeIcon,
+  PhotographIcon,
+  ChartSquareBarIcon,
+} from "@heroicons/vue/outline";
 
 export default createStore({
+  components: {},
   state: {
+    sidebarNavigation: [
+      { name: "Home", to: "/", icon: HomeIcon, current: true },
+      { name: "Photos", to: "/photos", icon: PhotographIcon, current: false },
+      { name: "Leaks", to: "/leaks", icon: ChartSquareBarIcon, current: false },
+      { name: "Trash", to: "/trash", icon: TrashIcon, current: false },
+      { name: "Settings", to: "/settings", icon: CogIcon, current: false },
+    ],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    changeCurrent(state, index) {
+      if (!state.sidebarNavigation[index].current) {
+        state.sidebarNavigation[index].current = true;
+
+        state.sidebarNavigation.forEach((element) =>
+          element !== state.sidebarNavigation[index]
+            ? (element.current = false)
+            : {}
+        );
+      }
+    },
+
+    setCurrentAtRefresh(state) {
+      state.sidebarNavigation.forEach((element, index) => {
+        if (
+          window.location.href.includes(element.to) &&
+          element.to.length > 1
+        ) {
+          state.sidebarNavigation[index].current = true;
+        } else if (window.location.href.endsWith("/")) {
+          state.sidebarNavigation[0].current = true;
+        } else {
+          state.sidebarNavigation[index].current = false;
+        }
+      });
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
